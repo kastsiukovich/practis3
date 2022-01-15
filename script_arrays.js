cd7de63037e1fa163fc94754a0db50030d4c650a
Alex <alex.kastsiukovich@gmail.com>
2022-01-16 00:24:29 +0300

    array

--- script_arrays.js
+++ script_arrays.js
@@ -6,7 +6,16 @@ arrayToString(["a", "b", "c", "d", "e", "f"]) ➞ "abcdef"
 
 arrayToString([1, 2, 3, "a", "s", "dAAAA"]) ➞ "123asdAAAA"*/
 
+// вар1
+function arrayToString(arr) {
+    var newStr = arr.split('');
+    return newStr;
+}
+console.log(arrayToString([1, 2, 3, 4, 5, 6]));
+console.log(arrayToString(["a", "b", "c", "d", "e", "f"]));
+console.log(arrayToString([1, 2, 3, "a", "s", "dAAAA"]))
 
+// вар 2
 function arrayToString(arr) {
     var resultStr = '';
     for (var i = 0; i < arr.length; i++) {
